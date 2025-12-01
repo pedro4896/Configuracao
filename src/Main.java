55658db285cb6c123630bc82509454a80c1b001e
Pedro Victor da Silva Pereira <pedrovictordasilvapereira4896@gmail.com>
2025-12-01 11:38:22 -0300

    feat: adicionar classe Main para demonstrar a leitura do Singleton

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        ConfigSingleton cfg = ConfigSingleton.getInstance();
+        ConfigFile file = cfg.getConfig();
+
+        System.out.println("--- CONFIGURAÇÕES CARREGADAS ---");
+        System.out.println("AppId: " + file.getAppId());
+        System.out.println("UserName: " + file.getUserName());
+        System.out.println("Language: " + file.getLanguage());
+        System.out.println("Offline: " + file.getOffline());
+        System.out.println("AutoDLC: " + file.getAutoDLC());
+        System.out.println("BuildId: " + file.getBuildId());
+        System.out.println("DLCName: " + file.getDlcName());
+        System.out.println("UpdateDB: " + file.getUpdateDB());
+        System.out.println("Signature: " + file.getSignature());
+        System.out.println("WindowInfo: " + file.getWindowInfo());
+        System.out.println("LVWindowInfo: " + file.getLvWindowInfo());
+        System.out.println("ApplicationPath: " + file.getApplicationPath());
+        System.out.println("WorkingDirectory: " + file.getWorkingDirectory());
+        System.out.println("WaitForExit: " + file.getWaitForExit());
+        System.out.println("NoOperation: " + file.getNoOperation());
     }
 }
